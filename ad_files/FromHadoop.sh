282ad3d9e0380dc7c5b06718995aa83239365295
MANASASRIRAMENENI <manasa.sriram@maplelabs.com>
2018-02-22 18:06:04 -0800

    Update FromHadoop.sh

--- ad_files/FromHadoop.sh
+++ ad_files/FromHadoop.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
- 
+
 export FROM_HADOOP_PROFILE=1
-uname=`whoami`
+uname=$(whoami)
 hdir=""
- 
-if [[ "\$uname" == "root" ]]
+
+if [[ $uname == "root" ]]
 then
-        hdir="/root"
+        exit
 else
-        hdir="/user/\$uname"
-fi
- 
-hadoop fs -ls -d \$hdir
-lsout=\`echo \$?\`
-echo \$lsout
-if [[ "\$lsout" != "0" ]]
-then
-      echo file does not exists
-      sudo -u hdfs hadoop fs -mkdir \$hdir
+        hdir="/user/$uname"
+        hadoop fs -ls -d $hdir
+        lsout=$(echo $?)
+        echo $lsout
+        if [[ $lsout != 0 ]]
+        then
+                echo file does not exists
+                sudo -u hdfs hadoop fs -mkdir $hdir
+                sudo -u hdfs hadoop fs -chown $uname $hdir
+                sudo -u hdfs hadoop fs -chmod 770 $hdir
+                fi
 fi
